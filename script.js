18f73f32eaaf4e335acbca9a7500025ecc84274c
Caio Freire Lima de Assis <caiofreire_1@hotmail.com>
2022-11-21 21:11:17 -0300

    fix(pomodoro):removendo WebkitAnimationDuration.

--- script.js
+++ script.js
@@ -96,13 +96,11 @@ const animations = document.querySelectorAll('circle#c2');
 const animations1 = document.querySelectorAll('circle#c1');
 animations.forEach(animation => {
   const running = animation.style.animationPlayState || 'paused';
-  animation.style.WebkitAnimationDuration = tempoRestante + 's';
   animation.style.animationDuration = tempoRestante + 's';
   animation.style.animationPlayState = 'running';
 })
 animations1.forEach(animation => {
   const running = animation.style.animationPlayState || 'paused';
-  animation.style.WebkitAnimationDuration = tempoRestante + 's';
   animation.style.animationDuration = tempoRestante + 's';
   animation.style.animationPlayState = 'running';
 })
